d17498cacbc88f0f3ed7d3b0f387f480af24ab9c
mochizuki-kazuki <mochizuki-kazuki@kayac.com>
2017-07-09 10:43:17 +0900

    update init-defaults

--- .emacs.d/conf/init-defaults.el
+++ .emacs.d/conf/init-defaults.el
@@ -7,6 +7,23 @@
 (set-language-environment 'Japanese)
 (prefer-coding-system 'utf-8)
 
+;; パスの設定
+(add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "~/bin")
+
+;; ファイル名の扱い
+;; Mac OS Xの場合のファイル名の設定
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+;; Windowsの場合のファイル名の設定
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+
+
 ;; ターミナル以外はツールバー、スクロールバーを非表示
 (when window-system
   ;; tool-barを非表示
