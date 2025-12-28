b3614c15fc898e5413843c115b992e6ff171343e
mczkzk <k.mochizuki.0409@gmail.com>
2025-12-28 22:28:29 +0900

    update dotfiles

--- dot_config/zsh/functions/media-viewer.zsh
+++ dot_config/zsh/functions/media-viewer.zsh
@@ -6,6 +6,10 @@ media-viewer() {
     cd "/Volumes/Extreme SSD/00_Memories/Selected_Media/media-viewer"
     node server.js &
     local server_pid=$!
+
+    # Trap Ctrl+C to properly kill server
+    trap "kill $server_pid 2>/dev/null; return" INT TERM
+
     sleep 2
     open http://localhost:9000
     wait $server_pid
