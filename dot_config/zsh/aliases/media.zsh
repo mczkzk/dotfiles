e28b07e046cb3b1f53827c9dee51bc26ddb1e53e
mczkzk <k.mochizuki.0409@gmail.com>
2025-12-28 17:19:32 +0900

    update dotfiles

---
+++ dot_config/zsh/aliases/media.zsh
@@ -0,0 +1,6 @@
+# ===============================
+# Media Viewer Aliases
+# ===============================
+
+# Start media viewer server from anywhere
+alias media-viewer='cd "/Volumes/Extreme SSD/00_Memories/Selected_Media/media-viewer" && node server.js'
